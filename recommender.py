1cd3bebc33f5f6274b88c2d6ff87cc87c149185f
c e smith <crazyfox@cs-MacBook.local>
2017-12-26 12:17:53 -0800

    start on refine branch

--- recommender.py
+++ recommender.py
@@ -1,22 +1,33 @@
+# This is the new test line for the new branch called 'refine'
 # Load external data file into "df"
 # Ensure proper header/index/column labeling as "userID", "gameID", "rating"
 # Call predict_rating(userID, gameID)
 # That's it! You can return the top eight suggested gameID with get_top_suggestions(userID)
-# Data objects are "df", "all_data_ptable", "ratings_array", "user_similarity", "user_prediction_matrix"
-# Data objects stay the same. New values are returned by all functions.
+
+# Try averaging predictions with average gameID rating
 import numpy
 import pandas
 
 # header = ['userID', 'gameID', 'rating']
-df = pandas.read_csv('inputs/withheld-ratings.csv').rename(columns = {'Compiled from boardgamegeek.com by Matt Borthwick':'userID'})
+header_test = ['userID', 'gameID']
+df1 = pandas.read_csv('inputs/boardgame-elite-users.csv').rename(columns = {'Compiled from boardgamegeek.com by Matt Borthwick':'userID'})
+df2 = pandas.read_csv('inputs/boardgame-frequent-users.csv').rename(columns = {'Compiled from boardgamegeek.com by Matt Borthwick':'userID'})
+frames = [df1, df2]
+df = pandas.concat(frames)
 
+my_text = numpy.loadtxt('inputs/boardgame-elite-users.csv', delimiter = ',', skiprows = 1, dtype = numpy.float16)
+userIDs = numpy.unique(my_text[:,0])
+gameIDs = numpy.unique(my_text[:,1])
+ratings = numpy.unique(my_text[:,2])
+
+# print(my_text)
+
+df_test_matrix = numpy.loadtxt('inputs/boardgame-users-test.csv', delimiter = ',')
 all_data_ptable = pandas.pivot_table(df, index='userID', columns='gameID', values='rating', fill_value=0)
 ratings_array = numpy.array(all_data_ptable)
 
-# For Testing
-df2 = pandas.read_csv('inputs/boardgame-elite-users.csv').rename(columns = {'Compiled from boardgamegeek.com by Matt Borthwick':'userID'})
-all_data_ptable2 = pandas.pivot_table(df2, index='userID', columns='gameID', values='rating', fill_value=0)
-ratings_array2 = numpy.array(all_data_ptable2)
+# from scipy.sparse import issparse
+# print(issparse(all_matrix))
 
 from sklearn.metrics.pairwise import pairwise_distances
 user_similarity = pairwise_distances(ratings_array, metric='cosine')
@@ -26,7 +37,8 @@ user_similarity = pairwise_distances(ratings_array, metric='cosine')
 def derive_prediction_matrix(ratings_arr, user_simil):
     mean_user_rating = ratings_arr.mean(axis=1)
     ratings_minus_mean = (ratings_arr - mean_user_rating[:, numpy.newaxis])
-    pred = mean_user_rating[:, numpy.newaxis] + user_simil.dot(ratings_minus_mean) / numpy.count_nonzero(numpy.array(ratings_arr).T, axis = 1)
+    # am I adding mean_user_rating back twice? this still doesn't make sense below
+    pred = mean_user_rating[:, numpy.newaxis] + user_simil.dot(ratings_minus_mean) / numpy.count_nonzero(ratings_arr.T, axis = 1)
     # Add back mean_user_rating
     pred = pred + mean_user_rating[:, numpy.newaxis]
     return pred
@@ -44,59 +56,33 @@ def max_ten(some_float):
     return 10.0 if some_float > 10 else some_float
 
 # Takes 2 ints, returns a float rounded to nearest half (1.0, 1.5, 2.0)
+# change this so I don't need to reference ptable
 def predict_rating(userID, gameID):
     user_location = numpy.where(all_data_ptable.index.values == userID)
     game_location = numpy.where(all_data_ptable.columns == gameID)
     return max_ten(round_to_p5(user_prediction_matrix[user_location, game_location]))
+print(all_data_ptable.index.values)
 
 # Takes an int and returns a list of 8 ints (gameIDs)
-def get_top_suggestions(userID):
-    rating_game_tuples = []
-    for gameID in all_data_ptable.columns:
-        if all_data_ptable.loc[(userID, gameID)] < .1:
-            rating = predict_rating(userID, gameID)
-            rating_game_tuples.append((rating, gameID))
-    # return gameID list of highest 8 predicted ratings
-    return [x[1] for x in sorted(rating_game_tuples, reverse = True)[:9]]
-    
-# Return a matrix with both predictions for unrated gameIDs AND ratings of gameIDs already rated
-def correct_for_existing_ratings(pred_matrix, ratings_arr):
-    new_pred = []
-    for i, vec in enumerate(ratings_arr):
-        for j, val in enumerate(ratings_arr[i]):
-            if ratings_arr[i][j] > 0:
-                new_pred.append(ratings_arr[i][j])
-            else:
-                new_pred.append(pred_matrix[i][j])
-    new_pred = numpy.array(new_pred)
-    new_pred.shape = pred_matrix.shape
-    return new_pred
+# def get_top_suggestions(userID):
+#     rating_game_tuples = []
+#     for gameID in df[:,1]:
+#         if all_data_ptable.loc[(userID, gameID)] < .1:
+#             rating = predict_rating(userID, gameID)
+#             rating_game_tuples.append((rating, gameID))
+#     # return gameID list of highest 8 predicted ratings
+#     return [x[1] for x in sorted(rating_game_tuples, reverse = True)[:9]]
 
+my_file = open("users.csv","w+")
 
+for elem in df_test_matrix:
+    if elem[0] in all_data_ptable.index.values:
+        if 6 < predict_rating(elem[0], elem[1]) < 9:
+            my_file.write(str(elem[0]) + ',' + str(elem[1]) + ',' + str(predict_rating(elem[0], elem[1])) + '\n')
+my_file.close()
 
-print(get_top_suggestions(388))
-print(predict_rating(388, 39463))
-print(predict_rating(388, 35677))
-print(predict_rating(388, 17226))
-print(predict_rating(388, 25613))
-print(predict_rating(388, 96848))
-print(predict_rating(430, 31260))
-print(predict_rating(430, 25613))
-print(predict_rating(430, 161936))
-print(predict_rating(430, 188))
-print(predict_rating(3080, 36932))
-print(predict_rating(3080, 9209))
-print(predict_rating(3080, 161936))
-print(predict_rating(3080, 90137))
-
-#**********************************************************************************************
-# TESTS / PRINTS / ASSERTS
-# print(list(zip([predict_rating(272, x) for x in train_data_ptable.columns], list(train_data_ptable.loc[(272,)]))))
 #**********************************************************************************************
 # EVALUATION, RMSE
-from math import sqrt
-def rmse(predictions, targets):
-    return numpy.sqrt(((predictions - targets) ** 2).mean())
-print('RMSE with all sevens is ', rmse(numpy.full(ratings_array.shape, 7), ratings_array2))
-print('RMSE with pred matrix is ', rmse(user_prediction_matrix, ratings_array2))
-print('RMSE with corrected pred matrix is ', rmse(correct_for_existing_ratings(user_prediction_matrix, ratings_array), ratings_array2))
+# from math import sqrt
+# def rmse(predictions, targets):
+#     return numpy.sqrt(((predictions - targets) ** 2).mean())
